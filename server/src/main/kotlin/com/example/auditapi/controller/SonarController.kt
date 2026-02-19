9efbd2619f3bbcc872526b434dfcc4b874970903
Eliska Pulcova <eliska.pulcova@quanti.cz>
2026-02-19 10:42:42 +0100

    add a new EP

--- server/src/main/kotlin/com/example/auditapi/controller/SonarController.kt
+++ server/src/main/kotlin/com/example/auditapi/controller/SonarController.kt
@@ -1,6 +1,9 @@
 package com.example.auditapi.controller
 
+import com.example.auditapi.domain.model.*
 import com.example.auditapi.service.SonarAnalysisService
+import com.fasterxml.jackson.databind.JsonNode
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -16,4 +19,29 @@ class SonarController(
     ) {
         service.analyzeAndCreate(serviceKey, projectKey, branch = "main") //main just for now
     }
+
+    @PostMapping(
+        "/create/analysis",
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE]
+    )
+    fun createAnalysis(
+        @RequestParam serviceKey: String,
+        @RequestParam projectKey: String,
+
+        @RequestPart("issues") issues: SonarIssuesResponse,
+        @RequestPart("measures") measures: SonarMeasuresResponse,
+        @RequestPart("qualityGate") qualityGate: SonarQualityGateResponse,
+        @RequestPart("componentTree") componentTree: SonarComponentTreeResponse
+    ): SonarAnalysisDocument {
+
+        return service.createAnalysis(
+            serviceKey,
+            projectKey,
+            issues,
+            measures,
+            componentTree,
+            qualityGate
+        )
+    }
+
 }
